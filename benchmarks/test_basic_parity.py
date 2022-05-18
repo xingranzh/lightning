794f4234abda054ab844292b4630765d0ad1bce8
Akihiro Nitta <akihiro@pytorchlightning.ai>
2022-05-18 08:00:15 -0400

    asdf

--- benchmarks/test_basic_parity.py
+++ benchmarks/test_basic_parity.py
@@ -1,5 +1,9 @@
+from typing import Tuple
 import gc
 import time
+import os
+import logging
+import urllib
 
 import numpy as np
 import torch
@@ -7,13 +11,122 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 from torchvision import models, transforms
-from torchvision.datasets import CIFAR10, MNIST
+from torchvision.datasets import CIFAR10
 
 from pytorch_lightning.core.module import LightningModule
 from pytorch_lightning.trainer.trainer import Trainer
 from pytorch_lightning.utilities.seed import seed_everything
 
 _PATH_DATASETS = "~/data/"
+
+
+class MNIST(Dataset):
+    """Customized `MNIST <http://yann.lecun.com/exdb/mnist/>`_ dataset for testing Pytorch Lightning without the
+    torchvision dependency.
+
+    Part of the code was copied from
+    https://github.com/pytorch/vision/blob/build/v0.5.0/torchvision/datasets/mnist.py
+
+    Args:
+        root: Root directory of dataset where ``MNIST/processed/training.pt``
+            and  ``MNIST/processed/test.pt`` exist.
+        train: If ``True``, creates dataset from ``training.pt``,
+            otherwise from ``test.pt``.
+        normalize: mean and std deviation of the MNIST dataset.
+        download: If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+
+    Examples:
+        >>> dataset = MNIST(".", download=True)
+        >>> len(dataset)
+        60000
+        >>> torch.bincount(dataset.targets)
+        tensor([5923, 6742, 5958, 6131, 5842, 5421, 5918, 6265, 5851, 5949])
+    """
+
+    RESOURCES = (
+        "https://pl-public-data.s3.amazonaws.com/MNIST/processed/training.pt",
+        "https://pl-public-data.s3.amazonaws.com/MNIST/processed/test.pt",
+    )
+
+    TRAIN_FILE_NAME = "training.pt"
+    TEST_FILE_NAME = "test.pt"
+    cache_folder_name = "complete"
+
+    def __init__(
+        self, root: str, train: bool = True, normalize: tuple = (0.1307, 0.3081), download: bool = True, **kwargs
+    ):
+        super().__init__()
+        self.root = root
+        self.train = train  # training set or test set
+        self.normalize = normalize
+
+        self.prepare_data(download)
+
+        data_file = self.TRAIN_FILE_NAME if self.train else self.TEST_FILE_NAME
+        self.data, self.targets = self._try_load(os.path.join(self.cached_folder_path, data_file))
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, int]:
+        img = self.data[idx].float().unsqueeze(0)
+        target = int(self.targets[idx])
+
+        if self.normalize is not None and len(self.normalize) == 2:
+            img = self.normalize_tensor(img, *self.normalize)
+
+        return img, target
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    @property
+    def cached_folder_path(self) -> str:
+        return os.path.join(self.root, "MNIST", self.cache_folder_name)
+
+    def _check_exists(self, data_folder: str) -> bool:
+        existing = True
+        for fname in (self.TRAIN_FILE_NAME, self.TEST_FILE_NAME):
+            existing = existing and os.path.isfile(os.path.join(data_folder, fname))
+        return existing
+
+    def prepare_data(self, download: bool = True):
+        if download and not self._check_exists(self.cached_folder_path):
+            self._download(self.cached_folder_path)
+        if not self._check_exists(self.cached_folder_path):
+            raise RuntimeError("Dataset not found.")
+
+    def _download(self, data_folder: str) -> None:
+        os.makedirs(data_folder, exist_ok=True)
+        for url in self.RESOURCES:
+            logging.info(f"Downloading {url}")
+            fpath = os.path.join(data_folder, os.path.basename(url))
+            urllib.request.urlretrieve(url, fpath)
+
+    @staticmethod
+    def _try_load(path_data, trials: int = 30, delta: float = 1.0):
+        """Resolving loading from the same time from multiple concurrent processes."""
+        res, exception = None, None
+        assert trials, "at least some trial has to be set"
+        assert os.path.isfile(path_data), f"missing file: {path_data}"
+        for _ in range(trials):
+            try:
+                res = torch.load(path_data)
+            # todo: specify the possible exception
+            except Exception as e:
+                exception = e
+                time.sleep(delta * random.random())
+            else:
+                break
+        if exception is not None:
+            # raise the caught exception
+            raise exception
+        return res
+
+    @staticmethod
+    def normalize_tensor(tensor: torch.Tensor, mean: float = 0.0, std: float = 1.0) -> torch.Tensor:
+        mean = torch.as_tensor(mean, dtype=tensor.dtype, device=tensor.device)
+        std = torch.as_tensor(std, dtype=tensor.dtype, device=tensor.device)
+        return tensor.sub(mean).div(std)
 
 
 class AverageDataset(Dataset):
@@ -225,6 +338,7 @@ def lightning_loop(cls_model, device_type: str = "cuda", num_epochs=10):
 
 
 class PyTorchParitySuite:
+
     def setup(self):
         if not torch.cuda.is_available():
             # skip this benchmark
@@ -235,21 +349,17 @@ class PyTorchParitySuite:
     # (ParityModuleRNN, 0.05, 0.001, 4, 3),
     # ParityModuleMNIST, 0.3, 0.001, 4, 3),  # todo: lower this thr
     # pytest.param(ParityModuleCIFAR, 4.0, 0.0002, 2, 2, marks=_MARK_SHORT_BM),
-    param_names = ["cls_model", "num_epochs"]
-    params = (ParityModuleMNIST, 4)
+    # param_names = ["cls_model", "num_epochs"]
+    # params = ([ParityModuleMNIST], [4])
 
-    def time_lightning(
-        self,
-        cls_model: LightningModule,
-        num_epochs: int,
-    ):
+    def time_lightning(self):
+        cls_model = ParityModuleMNIST
+        num_epochs = 4
         measure_loops(cls_model, kind="PT Lightning", num_epochs=num_epochs)
 
-    def time_pytorch(
-        self,
-        cls_model: LightningModule,
-        num_epochs: int,
-    ):
+    def time_pytorch(self):
+        cls_model = ParityModuleMNIST
+        num_epochs = 4
         measure_loops(cls_model, kind="Vanilla PT", num_epochs=num_epochs)
         # max_diff_speed: float,
         # max_diff_memory: float,
