b544220fa98927ea78ad2d751f18e2db2f35f8e8
awaelchli <aedu.waelchli@gmail.com>
2022-07-14 17:20:21 +0200

    fix merge conflict

--- tests/tests_pytorch/lite/test_lite.py
+++ tests/tests_pytorch/lite/test_lite.py
@@ -323,20 +323,22 @@ def test_to_device(accelerator, expected):
 
     class Lite(LightningLite):
         def run(self):
+            expected_device = torch.device(expected)
+
             # module
             module = torch.nn.Linear(2, 3)
             module = lite.to_device(module)
-            assert all(param.device == expected for param in module.parameters())
+            assert all(param.device == expected_device for param in module.parameters())
 
             # tensor
             tensor = torch.rand(2, 2)
             tensor = lite.to_device(tensor)
-            assert tensor.device == expected
+            assert tensor.device == expected_device
 
             # collection
             collection = {"data": torch.rand(2, 2), "int": 1}
             collection = lite.to_device(collection)
-            assert collection["data"].device == expected
+            assert collection["data"].device == expected_device
 
     lite = Lite(accelerator=accelerator, devices=1)
     lite.run()
